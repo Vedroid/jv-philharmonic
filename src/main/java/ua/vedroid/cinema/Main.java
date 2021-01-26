7f8b48efa474e2efee7a8ef13828b3934216c7db
Vadim <Vedroidua@gmail.com>
2021-01-26 19:34:21 +0200

    Added log4j

--- src/main/java/ua/vedroid/cinema/Main.java
+++ src/main/java/ua/vedroid/cinema/Main.java
@@ -1,11 +1,14 @@
 package ua.vedroid.cinema;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import ua.vedroid.cinema.lib.Injector;
 import ua.vedroid.cinema.model.Movie;
 import ua.vedroid.cinema.service.MovieService;
 
 public class Main {
     private static final Injector injector = Injector.getInstance("ua.vedroid.cinema");
+    private static final Logger log = LogManager.getLogger();
 
     public static void main(String[] args) {
         Movie movie = new Movie();
@@ -13,6 +16,6 @@ public class Main {
         MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
         movieService.add(movie);
 
-        movieService.getAll().forEach(System.out::println);
+        log.info("All movies: " + movieService.getAll());
     }
 }
--- src/main/java/ua/vedroid/cinema/dao/impl/MovieDaoImpl.java
+++ src/main/java/ua/vedroid/cinema/dao/impl/MovieDaoImpl.java
@@ -1,6 +1,8 @@
 package ua.vedroid.cinema.dao.impl;
 
 import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
@@ -12,6 +14,8 @@ import ua.vedroid.cinema.util.HibernateUtil;
 
 @Dao
 public class MovieDaoImpl implements MovieDao {
+    static final Logger log = LogManager.getLogger(MovieDaoImpl.class);
+
     @Override
     public Movie add(Movie movie) {
         Session session = null;
@@ -21,11 +25,13 @@ public class MovieDaoImpl implements MovieDao {
             transaction = session.beginTransaction();
             session.save(movie);
             transaction.commit();
+            log.info("Added new movie: " + movie);
             return movie;
         } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
             }
+            log.error("Can`t insert Movie entity", e);
             throw new DataProcessingException("Can`t insert Movie entity " + movie, e);
         } finally {
             if (session != null) {
@@ -40,6 +46,7 @@ public class MovieDaoImpl implements MovieDao {
             Query<Movie> getAllMoviesQuery = session.createQuery("from Movie", Movie.class);
             return getAllMoviesQuery.getResultList();
         } catch (Exception e) {
+            log.error("Error retrieving all movies", e);
             throw new DataProcessingException("Error retrieving all movies", e);
         }
     }
