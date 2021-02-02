544f033df1fc8bf2df291e49bf8a24caf9f1f8b8
Vadim <Vedroidua@gmail.com>
2021-02-02 13:26:31 +0200

    Created AuthenticationService

---
+++ src/main/java/ua/vedroid/cinema/exception/AuthenticationException.java
@@ -0,0 +1,7 @@
+package ua.vedroid.cinema.exception;
+
+public class AuthenticationException extends Exception {
+    public AuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
