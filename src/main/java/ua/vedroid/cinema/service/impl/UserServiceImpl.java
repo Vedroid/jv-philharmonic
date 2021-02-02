a5b1babb970824d29c342fbf1feff549b55c2f27
Vadim <Vedroidua@gmail.com>
2021-02-02 19:54:18 +0200

    Implements UserDao and UserService

--- src/main/java/ua/vedroid/cinema/service/impl/UserServiceImpl.java
+++ src/main/java/ua/vedroid/cinema/service/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import ua.vedroid.cinema.lib.Inject;
 import ua.vedroid.cinema.lib.Service;
 import ua.vedroid.cinema.model.User;
 import ua.vedroid.cinema.service.UserService;
+import ua.vedroid.cinema.util.HashUtil;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -15,6 +16,10 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User add(User user) {
+        byte[] salt = HashUtil.getSalt();
+        String hashedPassword = HashUtil.hashPassword(user.getPassword(), salt);
+        user.setPassword(hashedPassword);
+        user.setSalt(salt);
         return userDao.add(user);
     }
 
