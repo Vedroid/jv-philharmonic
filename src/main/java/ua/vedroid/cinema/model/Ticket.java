b3df6560036796a9f4c4efd6955c547531b2fece
Vadim <Vedroidua@gmail.com>
2021-02-04 11:50:35 +0200

    Change OneToOne -> ManyToOne

--- src/main/java/ua/vedroid/cinema/model/Ticket.java
+++ src/main/java/ua/vedroid/cinema/model/Ticket.java
@@ -6,6 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -18,8 +19,7 @@ public class Ticket {
     @ManyToOne
     @JoinColumn(name = "movie_session_id")
     private MovieSession movieSession;
-    @OneToOne
-    @JoinColumn(name = "user_id")
+    @ManyToOne
     private User user;
 
     public Long getId() {
