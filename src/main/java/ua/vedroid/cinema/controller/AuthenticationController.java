08899ddc6b3af0e89858f1f52fecb02d713252fa
Vadim <Vedroidua@gmail.com>
2021-02-15 12:45:40 +0200

    Added AuthenticationController

---
+++ src/main/java/ua/vedroid/cinema/controller/AuthenticationController.java
@@ -0,0 +1,26 @@
+package ua.vedroid.cinema.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import ua.vedroid.cinema.model.dto.UserRequestDto;
+import ua.vedroid.cinema.security.AuthenticationService;
+import ua.vedroid.cinema.service.mapper.UserMapper;
+
+@RestController
+public class AuthenticationController {
+    private final AuthenticationService service;
+    private final UserMapper mapper;
+
+    @Autowired
+    public AuthenticationController(AuthenticationService service, UserMapper mapper) {
+        this.service = service;
+        this.mapper = mapper;
+    }
+
+    @PostMapping("/register")
+    public void register(@RequestBody UserRequestDto dto) {
+        service.register(mapper.toEntity(dto));
+    }
+}
--- src/main/java/ua/vedroid/cinema/security/AuthenticationService.java
+++ src/main/java/ua/vedroid/cinema/security/AuthenticationService.java
@@ -6,5 +6,5 @@ import ua.vedroid.cinema.model.User;
 public interface AuthenticationService {
     User login(String email, String password) throws AuthenticationException;
 
-    User register(String email, String password);
+    User register(User user);
 }
