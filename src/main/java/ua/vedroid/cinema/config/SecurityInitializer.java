5992dfef829d9dc4d2c40a321eea6ef5f52b4f11
Vadim <Vedroidua@gmail.com>
2021-02-18 15:41:50 +0200

    Configured Spring Security

---
+++ src/main/java/ua/vedroid/cinema/config/SecurityInitializer.java
@@ -0,0 +1,6 @@
+package ua.vedroid.cinema.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
