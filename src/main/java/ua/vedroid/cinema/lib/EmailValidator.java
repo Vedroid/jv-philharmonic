859dcdb9ee6b66c12517ac1affcf9bf003943471
Vadim <Vedroidua@gmail.com>
2021-02-19 10:06:01 +0200

    Removed empty line

--- src/main/java/ua/vedroid/cinema/lib/EmailValidator.java
+++ src/main/java/ua/vedroid/cinema/lib/EmailValidator.java
@@ -4,7 +4,6 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class EmailValidator implements ConstraintValidator<Email, String> {
-
     private static final String EMAIL_REGEX = "\\w+@[a-z]+\\.[a-z]+";
 
     @Override
